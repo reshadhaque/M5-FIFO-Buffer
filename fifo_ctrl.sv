cd82a3cc5d6ecc64b06fdddc34aa372cb7c70231
Reshad Haque <reshadulhaque1@gmail.com>
2024-11-14 10:03:58 -0800

    Initial commit

---
+++ fifo_ctrl.sv
@@ -0,0 +1,93 @@
+//The FIFO Controller does not deal with the data, it only controlls the address because it used pointers
+//It does not matter what data you put in it
+
+module fifo_ctrl#(parameter ADDR_WIDTH = 3)(
+    input logic clk, reset,
+    input logic wr, rd,
+    output logic full, empty,
+    output logic [ADDR_WIDTH-1:0] w_addr,
+    output logic [ADDR_WIDTH-1:0] r_addr
+);
+
+logic [ADDR_WIDTH-1:0] wr_ptr, wr_ptr_next;
+logic [ADDR_WIDTH-1:0] rd_ptr, rd_ptr_next;
+
+logic full_next;
+logic empty_next;
+
+    //FIFO Buffer Controller
+
+    always_ff@(posedge clk, posedge reset)
+    begin
+        if(reset)
+            begin
+                wr_ptr<=0;
+                rd_ptr<=0;
+                full<=1'b0;
+                empty<=1'b1;
+            end
+        else
+            begin
+                wr_ptr<=wr_ptr+next;
+                rd_ptr<=rd_ptr_next;
+                full <= full_next;
+                empty<=empty_next;
+            end
+    end
+
+    //State logic
+    always_comb
+    begin
+    //defaults
+    wr_ptr_next = wr_ptr;
+    rd_ptr_next = rd_ptr;
+    full_next = full;
+    empty_next = empty;
+
+        case({wr, rd})
+            2'b01: //read case
+            begin
+                if(~empty)
+                begin
+                    rd_ptr_next = rd_ptr + 1;
+                    full_next = 1'b0;
+
+                    if(rd_ptr_next == wr_ptr)
+                        empty_next = 1'b1;
+                end
+            end
+            2'b10: //write case
+            begin
+                if(~full)
+                begin
+                    wr_ptr_next = wr_ptr+1;
+                    empty_next = 1'b0;
+
+                    if(wr_ptr_next == rd_ptr)
+                        full_next = 1'b1;
+                end
+            end 
+            2'b11: //read and write at the same time
+            begin
+                if(empty)
+                    begin
+                        wr_ptr_next = wr_ptr;
+                        rd_ptr_next = rd_ptr;
+                    end
+                else
+                    begin
+                        wr_ptr_next = wr_ptr+1;
+                        rd_ptr_next = rd_ptr+1;
+                    end
+
+            end
+            default: //2;b00
+        endcase
+
+        //outputs
+        assign w_addr = wr_ptr;
+        assign r_addr = rd_ptr;
+
+    end
+
+endmodule
---
+++ reg_file.sv
@@ -0,0 +1,21 @@
+module reg_file #(parameter ADDR_WIDTH = 3, DATA_WIDTH = 8)(
+    input logic clk,
+    input logic w_en,
+    input logic [ADDR_WIDTH-1:0] r_addr,
+    input logic [ADDR_WIDTH-1:0] w_addr,
+    input logic [DATA_WIDTH-1:0] w_data,
+    output logic [DATA_WIDTH-1:0] r_data
+);
+
+    logic [DATA_WIDTH-1:0] memory [0:2**ADDR_WIDTH-1];
+    
+    //Synchronous write
+    always_ff@(posedge clk)
+    begin
+        if(w_en)
+            memory[w_addr]<=w_data;
+    end
+    //Asynchronous read
+    assign r_data = memory[r_data];
+
+endmodule
